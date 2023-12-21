aed6f7ce3e8b8092898513350593bb3a090b366f
xnm-sdksdk <n.mendonca@protonmail.com>
2023-12-21 11:19:36 +0000

    merging to functions the definition of the shapes

--- main.js
+++ main.js
@@ -18,43 +18,62 @@ const controls = new OrbitControls(camera, renderer.domElement);
 const loader = new GLTFLoader();
 
 // Plane
-const planeGeometry = new THREE.PlaneGeometry(30, 30);
-const materialPlane = new THREE.MeshBasicMaterial({
-  color: 0xffff00,
-  side: THREE.DoubleSide,
-});
-const planeMesh = new THREE.Mesh(planeGeometry, materialPlane);
-planeMesh.rotateX(-Math.PI / 2);
-planeMesh.position.y = -5.5;
-scene.add(planeMesh);
 
-// First Base Cylinder
-const baseCylinderGeometry = new THREE.CylinderGeometry(5, 5, 10, 32);
-const materialCylinder = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cylinderMesh = new THREE.Mesh(baseCylinderGeometry, materialCylinder);
-scene.add(cylinderMesh);
+function PlaneConstructor() {
+  this.geometry = new THREE.PlaneGeometry(30, 30);
+  this.material = new THREE.MeshBasicMaterial({
+    color: 0xffff00,
+    side: THREE.DoubleSide,
+  });
+  this.mesh = new THREE.Mesh(this.geometry, this.material);
+  this.mesh.position.set(0, -5, 0);
+  this.mesh.rotation.x = -Math.PI / 2;
+  scene.add(this.mesh);
+}
+
+let customPositionPlane = new PlaneConstructor({
+  position: new THREE.Vector3(0, 0, 0),
+});
+
+PlaneConstructor.prototype.update = () => {};
+
+function CylinderConstructor() {
+  this.geometry = new THREE.CylinderGeometry(5, 5, 10, 32);
+  this.material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+  this.mesh = new THREE.Mesh(this.geometry, this.material);
+  scene.add(this.mesh);
+}
+
+let customPositionCylinder = new CylinderConstructor({
+  position: new THREE.Vector3(0, 0, 0),
+});
+
+CylinderConstructor.prototype.update = () => {};
 
 // First Articulation Cylinder
-const articulationCylinderGeometry = new THREE.CylinderGeometry(5, 5, 9, 32);
-const materialArticulationCylinder = new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-});
-const cylinderArticulationMesh = new THREE.Mesh(
-  articulationCylinderGeometry,
-  materialArticulationCylinder
-);
-cylinderArticulationMesh.position.y = 10;
-cylinderArticulationMesh.rotation.x = Math.PI / 2;
+// const articulationCylinderGeometry = new THREE.CylinderGeometry(5, 5, 9, 32);
+// const materialArticulationCylinder = new THREE.MeshBasicMaterial({
+//   color: 0xff0000,
+// });
+// const cylinderArticulationMesh = new THREE.Mesh(
+//   articulationCylinderGeometry,
+//   materialArticulationCylinder
+// );
+// cylinderArticulationMesh.position.y = 10;
+// cylinderArticulationMesh.rotation.x = Math.PI / 2;
 
-scene.add(cylinderArticulationMesh);
+// scene.add(cylinderArticulationMesh);
 
 camera.position.set(0, 20, 100);
 controls.update();
 
+// Render loop
 function animate() {
   requestAnimationFrame(animate);
   controls.update();
+  // Render the scene
   renderer.render(scene, camera);
 }
 
+// Start the animation loop
 animate();
